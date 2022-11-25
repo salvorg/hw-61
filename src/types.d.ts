c422a273f21245c213bead8364e5eb316c397247
Almaz Balbaev <kujitengu@gmail.com>
2022-11-25 16:04:42 +0600

    прописана типизация

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export interface ApiCountry {
+  name: string;
+  capital: string;
+  population: string;
+  borders: [];
+  alpha3Code: string;
+  flag: string;
+}
+
+export interface BorderedCountry {
+  name: string;
+}
